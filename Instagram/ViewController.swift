2e8906f70df6f36f5dc19f94a8cb311bc38744b5
Nicolas Desormiere <nicolas.desormiere@gmail.com>
2018-08-11 19:47:20 +1000

    first commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Nicolas Desormiere on 10/8/18.
+//  Copyright © 2018 Nicolas Desormiere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
