35ce264fe38316c35353f990b194146c33a43130
Nicolas Desormiere <nicolas.desormiere@gmail.com>
2018-10-15 03:09:24 +1100

    add imagePickerController

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -8,14 +8,40 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
   let plusPhotoButton: UIButton = {
     let button = UIButton(type: .system)
     button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+    button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
     return button
   }()
+  
+  @objc func handlePlusPhoto() {
+    let imagePickerController = UIImagePickerController()
+    imagePickerController.delegate = self
+    imagePickerController.allowsEditing = true
+    
+    present(imagePickerController, animated: true, completion: nil)
+  }
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+      plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+    } else if let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+      plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+    
+    plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width/2
+    plusPhotoButton.layer.masksToBounds = true
+    plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+    plusPhotoButton.layer.borderWidth = 3
+    
+    dismiss(animated: true, completion: nil)
+  }
   
   let emailTextField: UITextField = {
     let textField = UITextField()
@@ -83,6 +109,50 @@ class ViewController: UIViewController {
         return
       }
       print("Successfully created user:", user?.user.uid ?? "")
+      
+      guard let image = self.plusPhotoButton.imageView?.image else { return }
+      
+      guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else { return }
+      
+      let filename = NSUUID().uuidString
+      
+      let storageRef = Storage.storage().reference().child("profile_images").child(filename)
+      storageRef.putData(uploadData, metadata: nil, completion: { (metadata, err) in
+        
+        if let err = err {
+          print("Failed to upload profile image:", err)
+          return
+        }
+        
+        // Firebase 5 Update: Must now retrieve downloadURL
+        storageRef.downloadURL(completion: { (downloadURL, err) in
+          if let err = err {
+            print("Failed to fetch downloadURL:", err)
+            return
+          }
+          
+          guard let profileImageUrl = downloadURL?.absoluteString else { return }
+          
+          print("Successfully uploaded profile image:", profileImageUrl)
+          
+          guard let uid = user?.user.uid else { return }
+          
+          let dictionaryValues = ["username": username, "profileImageUrl": profileImageUrl]
+          let values = [uid: dictionaryValues]
+          
+          Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+            
+            if let err = err {
+              print("Failed to save user info into db:", err)
+              return
+            }
+            
+            print("Successfully saved user info to db")
+            
+          })
+        })
+      })
+      
     })
   }
   
